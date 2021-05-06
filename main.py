d686dd11efe5128bbc8c89b92be9ab781ec7143e
JennaB17 <>
2021-05-06 17:36:16 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,84 @@
+#first progam
+#first = input('What is your name? ')
+#second = input('What is your last name? ')
+#print(second, first)
+
+
+
+#second program
+#num = int(input())
+#if (num // 2):
+  #print('This number is even')
+#elif num == 0:
+  #print('This is zero')
+#else:
+  #print('This number is odd')
+
+
+
+
+  #third program
+#date = int(input())
+#if date < 32:
+  #print("January", date)
+#if 32 < date < 60 :
+  #print("Febuary", date - 32)
+#if  60< date < 91:
+  #print("March", date - 60)
+#if 91< date <121:
+  #print("April", date-91)
+#if 121<date<152:
+  #print("May", date - 121)
+#if 152<date<182:
+  #print("June", date - 152)
+#if 182<date<213:
+  #print("July", date - 182)
+#if 213<date<244:
+  #print("Augaust", date - 213)
+#if 244<date<274:
+  #print("September", date-244)
+#if 275<date<305:
+  #print("October", date - 275)
+#if 305<date<335:
+  #print("November", date- 305)
+#if 335<date<365:
+  #print("December", date - 335)
+
+
+
+
+
+#fourth program
+#num = 5
+#for n in range(1, num+1):
+  #for i in range(num, n-1, -1):
+    #print(i, end="")
+  #print()
+
+
+  #fifth program
+#num = int(input())
+#for n in range(1, num +1):
+  #for i in range(num, n-1, -1):
+    #print(i, end="")
+  #print()
+
+
+  #sixth program
+  #Write a number between 1-7 in as your input(). 
+#day = int(input())
+#if day == 1:
+  #print("It is Monday!")
+#if day == 2:
+  #print("It is Tuesday!")
+#if day == 3:
+  #print("It is Wednesday!")
+#if day == 4:
+  #print("Its is Thursday")
+#if day == 5:
+  #print("It is Friday!")
+#if day == 6:
+  #print("Its is Saturday!")
+#if day == 7:
+  #print("It is Sunday!")
+
